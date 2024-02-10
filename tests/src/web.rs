d301800910fd449fbca0eaf573eea95019ec9222
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2024-02-10 06:38:13 +0100

    Complete tests for features that don't need mocks

--- tests/src/web.rs
+++ tests/src/web.rs
@@ -26,6 +26,20 @@ macro_rules! unmount {
     }};
 }
 
+async fn sleep(delay: i32) {
+    let mut cb = |resolve: js_sys::Function, _reject: js_sys::Function| {
+        web_sys::window()
+            .unwrap()
+            .set_timeout_with_callback_and_timeout_and_arguments_0(
+                &resolve, delay,
+            )
+            .unwrap();
+    };
+
+    let p = js_sys::Promise::new(&mut cb);
+    wasm_bindgen_futures::JsFuture::from(p).await.unwrap();
+}
+
 fn p_text() -> String {
     document()
         .query_selector("p")
@@ -42,28 +56,69 @@ fn input_by_id(id: &str) -> web_sys::HtmlInputElement {
         .unchecked_into::<web_sys::HtmlInputElement>()
 }
 
-#[wasm_bindgen_test]
-fn minimal_example() {
-    mount!(MinimalExampleApp);
+fn html() -> web_sys::HtmlHtmlElement {
+    document()
+        .document_element()
+        .unwrap()
+        .unchecked_into::<web_sys::HtmlHtmlElement>()
+}
 
+fn html_lang() -> String {
+    html().lang()
+}
+
+#[wasm_bindgen_test]
+async fn minimal_example() {
+    mount!(MinimalExampleApp);
+    let es = input_by_id("es");
+    let en = input_by_id("en");
+
+    // translations working
     assert_eq!(p_text(), "Select a language:");
-    input_by_id("es").click();
-    assert!(input_by_id("es").checked());
-    assert!(!input_by_id("en").checked());
+    es.click();
+    assert!(es.checked());
+    assert!(!en.checked());
     assert_eq!(p_text(), "Selecciona un idioma:");
+
+    // language change not reflected in html tag
+    sleep(30).await;
+    html().remove_attribute("lang").unwrap();
+    assert_eq!(html_lang(), "".to_string());
+    es.click();
+    assert!(es.checked());
+    assert_eq!(html_lang(), "".to_string());
+    en.click();
+    assert!(en.checked());
+    assert_eq!(html_lang(), "".to_string());
 
     unmount!();
 }
 
 #[wasm_bindgen_test]
-fn complete_example() {
+async fn complete_example() {
     mount!(CompleteExampleApp);
+    let es = input_by_id("es");
+    let en = input_by_id("en");
 
+    // translations working
     assert_eq!(p_text(), "Select a language:");
-    input_by_id("es").click();
-    assert!(input_by_id("es").checked());
-    assert!(!input_by_id("en").checked());
+    es.click();
+    assert!(es.checked());
+    assert!(!en.checked());
     assert_eq!(p_text(), "Selecciona un idioma:");
+    en.click();
+    assert!(en.checked());
+    assert!(!es.checked());
+    assert_eq!(p_text(), "Select a language:");
+
+    // language change reflected in html tag
+    sleep(30).await;
+    assert_eq!(html_lang(), "en".to_string());
+    es.click();
+    assert!(es.checked());
+    assert_eq!(html_lang(), "es".to_string());
+    en.click();
+    assert_eq!(html_lang(), "en".to_string());
 
     unmount!();
 }
