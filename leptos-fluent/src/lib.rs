410fbb4b621936b35a890b1d8f3983c1f06210d7
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2024-05-20 01:47:51 +0200

    Add unit test to check that version of README is updated (#63)

--- leptos-fluent/src/lib.rs
+++ leptos-fluent/src/lib.rs
@@ -381,3 +381,37 @@ pub fn language_from_str_between_languages(
         Err(_) => None,
     }
 }
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_readme_leptos_fluent_version_is_updated() {
+        let this_file = include_str!("./lib.rs");
+        let mut version = "";
+        for line in this_file.lines() {
+            if line.starts_with("//! leptos-fluent = ") {
+                version = line
+                    .split("leptos-fluent = \"")
+                    .nth(1)
+                    .unwrap()
+                    .split('"')
+                    .next()
+                    .unwrap();
+                break;
+            }
+        }
+
+        assert!(
+            !version.is_empty(),
+            "leptos-fluent = \"{version}\" not found in leptos-fluent/src/lib.rs"
+        );
+        assert_eq!(
+            version,
+            env!("CARGO_PKG_VERSION"),
+            concat!(
+                "The version of leptos-fluent shown in the README at",
+                " 'Installation' section is not updated."
+            ),
+        );
+    }
+}
