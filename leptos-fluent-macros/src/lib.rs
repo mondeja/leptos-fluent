d4de7a97d270ada3723d5aab607d11fc4702fa70
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2024-06-25 05:14:33 +0200

    Fix error

--- leptos-fluent-macros/src/lib.rs
+++ leptos-fluent-macros/src/lib.rs
@@ -310,7 +310,7 @@ pub fn leptos_fluent(
     #[cfg(all(not(feature = "system"), not(feature = "ssr")))]
     let initial_language_from_system_quote = quote! {};
 
-    #[cfg(feature = "ssr")]
+    #[cfg(all(feature = "system", feature = "ssr"))]
     {
         _ = initial_language_from_system_bool;
         _ = initial_language_from_system_expr;
