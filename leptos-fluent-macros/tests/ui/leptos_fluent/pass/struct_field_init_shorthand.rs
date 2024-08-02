723cb68ca66e2c1a803cbe23dcd3fbd270d38afa
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2024-08-02 14:53:08 +0200

    Allow struct field init shorthand for most macro parameters (#195)

---
+++ leptos-fluent-macros/tests/ui/leptos_fluent/pass/struct_field_init_shorthand.rs
@@ -0,0 +1,29 @@
+use fluent_templates::static_loader;
+use leptos::*;
+use leptos_fluent_macros::leptos_fluent;
+
+static_loader! {
+    pub static TRANSLATIONS = {
+        locales: "../../../../examples/csr-minimal/locales",
+        fallback_language: "en",
+    };
+}
+
+#[component]
+pub fn App() -> impl IntoView {
+    let cookie_name = "my_cookie";
+    let initial_language_from_cookie = true;
+
+    let i18n = leptos_fluent! {{
+        translations: [TRANSLATIONS],
+        cookie_name,
+        locales: "../../../../examples/csr-minimal/locales",
+        initial_language_from_cookie,
+    }};
+
+    logging::log!("I18n context: {:?}", i18n.meta().unwrap());
+
+    view! { <p>Foo</p> }
+}
+
+fn main() {}
