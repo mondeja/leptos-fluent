8d5262c6c6e6f34a6fb081c671d8c49fd451872a
Nicolas Cura <nicolas.cura@icloud.com>
2024-10-02 10:31:15 +0200

    Setup axum island base example

---
+++ examples/ssr-hydrate-axum-islands/src/lib.rs
@@ -0,0 +1,13 @@
+#[allow(unused_imports)]
+pub mod app;
+pub mod error_template;
+#[cfg(feature = "ssr")]
+pub mod fileserv;
+
+#[cfg(feature = "hydrate")]
+#[wasm_bindgen::prelude::wasm_bindgen]
+pub fn hydrate() {
+    use crate::app::*;
+    console_error_panic_hook::set_once();
+    leptos::leptos_dom::HydrationCtx::stop_hydrating();
+}
