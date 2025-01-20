d90e4ef19ca19771e04fcbf8b39683f7cd6bb22e
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2025-01-20 15:57:11 +0100

    Run end2end tests with browser preferences

---
+++ end2end/accept-language-header/tests/initial_language_from_accept_language_header.rs
@@ -0,0 +1,28 @@
+use leptos::prelude::*;
+use leptos_fluent::leptos_fluent;
+use leptos_fluent_csr_minimal_example::{LanguageSelector, TRANSLATIONS};
+use tests_helpers::{element_text, input_by_id, mount, unmount};
+use wasm_bindgen_test::*;
+
+wasm_bindgen_test_configure!(run_in_browser);
+
+#[component]
+pub fn App() -> impl IntoView {
+    leptos_fluent! {
+        translations: [TRANSLATIONS],
+        locales: "../../examples/csr-minimal/locales",
+        initial_language_from_accept_language_header: true,
+    };
+
+    view! { <LanguageSelector /> }
+}
+
+#[wasm_bindgen_test]
+async fn test_initial_language_from_accept_language_header() {
+    let es = move || input_by_id("es");
+
+    mount!(App);
+    assert!(es().checked());
+    assert_eq!(element_text("p"), "Selecciona un idioma:");
+    unmount!();
+}
