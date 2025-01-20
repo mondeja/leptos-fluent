5cb0d1e11b506443e0403df677626b0f7b001ecc
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2025-01-20 17:42:20 +0100

    Add end2end tests with custom browser preferences (#277)

--- end2end/TODO.md
+++ end2end/TODO.md
@@ -2,12 +2,8 @@
 
 ## Macro parameters
 
-- `initial_language_from_navigator`
 - `initial_language_from_accept_language_header`
-- `set_language_to_cookie`
 - `cookie_attrs`
-- `initial_language_from_navigator_to_localstorage`
-- `initial_language_from_navigator_to_cookie`
 - `initial_language_from_system`
 - `set_language_to_data_file`
 - `initial_language_from_data_file`
---
+++ end2end/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+EXITCODE=0
+BROWSER=$1
+
+set -x
+cd end2end
+wasm-pack test --headless --$BROWSER
+if [ $? -ne 0 ]; then
+  EXITCODE=1
+fi
+
+for dir in */; do
+  if [ "$dir" = "tests-helpers/" ] || [ "$dir" = "tests/" ]; then
+    continue;
+  fi
+  cd $dir
+  wasm-pack test --headless --$BROWSER
+  if [ $? -ne 0 ]; then
+    EXITCODE=1
+  fi
+  cd ..
+done
+
+cd ..
+set +x
+
+exit $EXITCODE
