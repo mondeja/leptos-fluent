929ed46b9768cae967694eaa9e8e6e07aded51e9
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2025-05-06 13:21:06 +0200

    Refactor Axum example

--- examples/ssr-hydrate-axum/src/main.rs
+++ examples/ssr-hydrate-axum/src/main.rs
@@ -1,31 +1,31 @@
-use axum::Router;
-use leptos::prelude::*;
-use leptos_axum::{generate_route_list, LeptosRoutes};
-use leptos_fluent_ssr_hydrate_axum_example::app::{shell, App};
-
+#[cfg(feature = "ssr")]
 #[tokio::main]
 async fn main() {
+    use leptos::logging::log;
+    use leptos::prelude::get_configuration;
+    use leptos_axum::{
+        file_and_error_handler, generate_route_list, LeptosRoutes,
+    };
+    use leptos_fluent_ssr_hydrate_axum_example::app::{shell, App};
+
     let conf = get_configuration(None).unwrap();
     let leptos_options = conf.leptos_options;
     let addr = leptos_options.site_addr;
     let routes = generate_route_list(App);
 
     // build our application with a route
-    let app = Router::new()
+    let app = axum::Router::new()
         .leptos_routes(&leptos_options, routes, {
             let leptos_options = leptos_options.clone();
             move || shell(leptos_options.clone())
         })
-        .fallback(leptos_axum::file_and_error_handler(shell))
+        .fallback(file_and_error_handler(shell))
         .with_state(leptos_options);
 
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
     let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
-    #[allow(clippy::print_stdout)]
-    {
-        println!("listening on http://{}", &addr);
-    };
+    log!("listening on http://{}", &addr);
     axum::serve(listener, app.into_make_service())
         .await
         .unwrap();
