25fba8469ca7904523086c3e8ad362660efd1ff0
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2025-10-20 14:38:23 +0200

    Fix error in test

--- tests/end2end/ssr/src/app-helpers/src/lib.rs
+++ tests/end2end/ssr/src/app-helpers/src/lib.rs
@@ -167,12 +167,11 @@ pub mod actix {
                 .await
             }
 
-            #[cfg(not(any(feature = "ssr", feature = "csr")))]
+            #[cfg(not(feature = "ssr"))]
             pub fn main() {
                 // no client-side main function
                 // unless we want this to work with e.g., Trunk for pure client-side testing
                 // see lib.rs for hydration function instead
-                // see optional feature `csr` instead
             }
         };
     }
