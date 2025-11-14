ecbe5e23eb2c622cd45fbaa12c70ab0fb2d9e72c
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2025-11-14 18:43:39 +0000

    Add integration test

---
+++ leptos-fluent-macros/tests/ui/leptos_fluent/stable/pass/locales_scripts.rs
@@ -0,0 +1,23 @@
+use leptos::prelude::*;
+use leptos_fluent::{leptos_fluent, move_tr};
+
+#[component]
+fn I18n(children: Children) -> impl IntoView {
+    leptos_fluent! {
+        children: children(),
+        default_language: "sr-Latn",
+        locales: "../../../../leptos-fluent-macros/tests/ui/leptos_fluent/stable/pass/locales_scripts",
+        default_language: "sr-Latn",
+    }
+}
+
+#[component]
+pub fn App() -> impl IntoView {
+    view! {
+        <I18n>
+            <p>{move_tr!("hello")}</p>
+        </I18n>
+    }
+}
+
+fn main() {}
